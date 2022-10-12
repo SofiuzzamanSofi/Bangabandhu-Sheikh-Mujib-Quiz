4a805438fbb7801c3d1845e2c1b6eee8a8aba53b
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-10-12 12:52:22 +0600

    dynamic link clicked

--- src/component/Quiz/Quiz.jsx
+++ src/component/Quiz/Quiz.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Quiz = ({ quiz }) => {
-    const { id, logo, name, total } = quiz
-    console.log(quiz)
+    const { id, logo, name, total } = quiz;
+
+
     return (
         <div className='rounded-md  bg-cyan-900 border w-[340px] md:w-[500px]'>
             <div className='  flex flex-col  gap-4 w-[320px] m-[10px] md:w-[450px] md:m-[25px]'>
@@ -11,7 +14,7 @@ const Quiz = ({ quiz }) => {
                     <p>{name}</p>
                     <p> Total Questions: {total}</p>
                 </div>
-                <button className='hover:cursor-pointer hover:bg-green-700 bg-rose-700 py-4 w-full rounded font-bold'>Play Quiz</button>
+                <Link to={`question/${id}`}><button className='hover:cursor-pointer hover:bg-green-700 bg-rose-700 py-4 w-full rounded font-bold'>Play Quiz</button></Link>
 
             </div>
         </div>
--- src/layout/routes.js
+++ src/layout/routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import About from "../component/About/About";
 import Blog from "../component/Blog/Blog";
 import ErrorPage from "../component/ErrorPage/ErrorPage";
+import Questions from "../component/Questins/Questions";
 import Root from "../component/Root/Root";
 import Statistics from "../component/Statistics/Statistics";
 import Topic from "../component/Topic/Topic";
@@ -18,7 +19,8 @@ const router = createBrowserRouter([
             { path: 'topic', element: <Topic /> },
             { path: 'statistics', element: <Statistics /> },
             { path: 'statistics', element: <Statistics /> },
-            { path: 'blog', element: <Blog /> }
+            { path: 'blog', element: <Blog /> },
+            { path: 'question/:id', element: <Questions /> },
         ]
     }
 ])
